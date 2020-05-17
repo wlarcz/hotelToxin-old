8bfb318bb4e74f04d3d6ac7c29ae6fd88b3c00f8
Wladyslaw <kvazid@gmail.com>
2020-05-17 16:26:42 +0700

    Подключил bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import "./styles.css"
+
+import 'bootstrap';
 
 console.log("hello world!")
 
 jQuery(document).ready(function($) {
-    $('body').css('background-color', 'orange');
+    //$('body').css('background-color', 'orange');
 });
